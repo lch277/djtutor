94827cdd6f6125c5302f27be92fc4f99c034f77e
lch277 <lch277@qq.com>
2018-11-02 00:16:24 +0800

    完善登录页

--- home/urls.py
+++ home/urls.py
@@ -20,6 +20,6 @@ app_name = 'home'
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home/index.html'), name='index'),
     path('about/', TemplateView.as_view(template_name='home/about.html'), name='about'),
-    # path('login', TemplateView.as_view(template_name='registration/login.html'), name='login'),
+    path('quote/', TemplateView.as_view(template_name='home/quote.html'), name='login'),
 ]
 
