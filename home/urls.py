b3fa7d1e22e8e766115469123b104d06cc42389b
水西 <lch277@qq.com>
2018-11-02 18:08:17 +0800

    报价页+报价模型整理

--- home/urls.py
+++ home/urls.py
@@ -19,7 +19,8 @@ from django.views.generic import TemplateView
 app_name = 'home'
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home/index.html'), name='index'),
+    path('quote/', TemplateView.as_view(template_name='home/quote.html'), name='quote'),
+    path('download/', TemplateView.as_view(template_name='home/download.html'), name='download'),
     path('about/', TemplateView.as_view(template_name='home/about.html'), name='about'),
-    path('quote/', TemplateView.as_view(template_name='home/quote.html'), name='login'),
 ]
 
